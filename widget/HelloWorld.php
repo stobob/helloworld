e001353a93c56125f0b70a91ea81dae7fcb06a88
Bob <bobby@sourcetoad.com>
2016-03-29 20:02:38 -0400

    initial commit

---
+++ widget/HelloWorld.php
@@ -0,0 +1,11 @@
+<?php 
+namespace stobob\helloworld\widgets;
+class HelloWorld extends yii\base\Widget {
+    /**
+    *
+    * [type] [description]
+    */
+    public function run(){
+        return "Hello World";
+    }
+}
